6f3d33127b32a65fa571feb4529d663a6929b7d2
Nigel Charman <nigel.charman.nz@gmail.com>
2015-04-09 09:46:11 +1200

    Publish of Github pages from Gradle.

--- spec/myCustomJsDirectory/collapse-expand.js
+++ spec/myCustomJsDirectory/collapse-expand.js
@@ -1,14 +1,10 @@
-var Collapsible = (function() {
-	var my = {};
-	my.collapseExpand = function(elementId) {
-		var ele = document.getElementById(elementId);
-		var collapsedHeight = parseInt(ele.getAttribute("collapsed-height"));
-		var collapsedHeightUnit = ele.getAttribute("collapsed-height-unit");
-		if (parseInt(ele.style.height, 10) !== collapsedHeight) {
-			ele.style.height = collapsedHeight + collapsedHeightUnit;
-		} else {
-			ele.style.height = ele.scrollHeight;
-		}
+function collapseExpand(elementId) {
+	var ele = document.getElementById(elementId);
+	var collapsedHeight = parseInt(ele.getAttribute("collapsed-height"));
+	var collapsedHeightUnit = ele.getAttribute("collapsed-height-unit");
+	if (parseInt(ele.style.height, 10) !== collapsedHeight) {
+		ele.style.height = collapsedHeight + collapsedHeightUnit;
+	} else {
+		ele.style.height = ele.scrollHeight;
 	}
-	return my;
-})();
+}
